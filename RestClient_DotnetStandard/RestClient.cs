a6ef674c886236d84d85a11602722e471b29fc1a
gassen.benhadj <gassen.benhadj@outlook.com>
2018-01-24 23:15:16 +0100

    Add project files.

---
+++ RestClient_DotnetStandard/RestClient.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace RestClient_DotnetStandard
+{
+    public class RestClient<T>
+    {
+        public List<T> Get(string WebServiceUrl)
+        {
+            var httpClient = new HttpClient();
+
+            var json = httpClient.GetStringAsync(WebServiceUrl).Result;
+
+            var taskModels = JsonConvert.DeserializeObject<List<T>>(json);
+
+            return taskModels;
+        }
+
+        public  bool Post(string WebServiceUrl,T t)
+        {
+            var httpClient = new HttpClient();
+
+            var json = JsonConvert.SerializeObject(t);
+
+            HttpContent httpContent = new StringContent(json);
+
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var result =  httpClient.PostAsync(WebServiceUrl, httpContent).Result;
+
+            return result.IsSuccessStatusCode;
+        }
+
+        public bool Put(string WebServiceUrl,int id, T t)
+        {
+            var httpClient = new HttpClient();
+
+            var json = JsonConvert.SerializeObject(t);
+
+            HttpContent httpContent = new StringContent(json);
+
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var result =  httpClient.PutAsync(WebServiceUrl + id, httpContent).Result;
+
+            return result.IsSuccessStatusCode;
+        }
+
+        public bool Delete(string WebServiceUrl,int id, T t)
+        {
+            var httpClient = new HttpClient();
+
+            var response =  httpClient.DeleteAsync(WebServiceUrl + id).Result;
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
